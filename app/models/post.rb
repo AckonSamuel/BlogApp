6fd6fb8c6760e31e110e3a8c926b74e05992a503
Samuel Ackon <ackons433@gmail.com>
2022-10-21 10:06:28 +0000

    Add model for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
     belongs_to :user, class_name: 'User'
     has_many :likes
     has_many :comments
-end
+    after_save :update_posts_counter
+  
+    def most_recent_comments
+      comments.order(created_at: :desc).limit(5)
+    end
+  
+    private
+  
+    def update_posts_counter
+      user.increment!(:posts_counter)
+    end
+  end
