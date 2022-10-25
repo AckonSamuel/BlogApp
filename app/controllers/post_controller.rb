732dff162cdf2448e7528ab7d6aa63c9771cd0fd
Samuel Ackon <ackons433@gmail.com>
2022-10-25 23:41:19 +0000

    Add post controller and view pages

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+    def index
+        @user = User.find(param[:user_id])
+        @post = Post.where(user_id: param[@user.id])
+    end
+
+    def display
+        @posts = Post.find(param[:id])
+    end
+end
