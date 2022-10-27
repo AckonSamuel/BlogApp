0bdc9f27ac5442e28cd3f40ffb6f17d5f1fa1c63
Samuel Ackon <ackons433@gmail.com>
2022-10-27 11:25:17 +0000

    Add user specific tests

--- spec/request/posts_spec.rb
+++ spec/request/posts_spec.rb
@@ -17,4 +17,23 @@ Rspec.describe 'Users', type: :request do
         it 'Checks the placeholder text' do
             expect(response.body).to include('<h1>Users</h1>')
         end
+    end
+
+    describe 'Check specific user' do
+        before(:example) do
+            get '/users/1'
+        end
+
+        it 'Checks if the status response is success(OK)' do
+            expect(response.status).to be(200)
+        end
+
+        it 'Checks whether it renders the correct template' do
+            expect(respnse).to render_template(:show)
+        end
+
+        it 'Checks presence of expected placeholder text' do
+            expect.(response.body).to include('<h2>users display</h2>')
+        end
+    end
 end
