ae5d8e4b454bfc447c2d7e2bb3596cac766f5032
Samuel Ackon <ackons433@gmail.com>
2022-10-27 16:36:13 +0000

    Add tests for users controller index

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/users"
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns http success" do
+      get "/users"
+      expect(response).to render_template(:index)
+    end
+
+    it "returns http success" do
+      get "/users"
+      expect(response.body).to include('<h1>Users</h1>')
+    end
+    
+  end
+
+end
