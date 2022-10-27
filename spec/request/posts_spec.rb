83ad8524a8fa381b80db6cd2db0acda5043db82f
Samuel Ackon <ackons433@gmail.com>
2022-10-27 13:08:36 +0000

    Fix linter errors

--- spec/request/posts_spec.rb
+++ spec/request/posts_spec.rb
@@ -1,38 +1,38 @@
 require_relative './../rails_helper'
 RSpec.describe 'Users', type: :request do
-    describe 'Get and checks User ' do
-        before(:example) do
-            get '/users'
-        end
-
-        it 'Checks if the status response is success(OK)' do
-            expect(response.status).to be(200)
-        end
-
-        it 'Checks if it render the correct template' do
-            expect(response).to render_template(:index)
-        end
-
-        it 'Checks the placeholder text' do
-            expect(response.body).to include('<h1>Users</h1>')
-        end
+  describe 'Get and checks User ' do
+    before(:example) do
+      get '/users'
     end
 
-    describe 'Check specific user' do
-        before(:example) do
-            get '/users/1'
-        end
-
-        it 'Checks if the status response is success(OK)' do
-            expect(response.status).to be(200)
-        end
-
-        it 'Checks whether it renders the correct template' do
-            expect(respnse).to render_template(:show)
-        end
-
-        it 'Checks presence of expected placeholder text' do
-            expect.(response.body).to include('<h2>users display</h2>')
-        end
+    it 'Checks if the status response is success(OK)' do
+      expect(response.status).to be(200)
     end
+
+    it 'Checks if it render the correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'Checks the placeholder text' do
+      expect(response.body).to include('<h1>Users</h1>')
+    end
+  end
+
+  describe 'Check specific user' do
+    before(:example) do
+      get '/users/1'
+    end
+
+    it 'Checks if the status response is success(OK)' do
+      expect(response.status).to be(200)
+    end
+
+    it 'Checks whether it renders the correct template' do
+      expect(respnse).to render_template(:show)
+    end
+
+    it 'Checks presence of expected placeholder text' do
+      expect.call(response.body).to include('<h2>users display</h2>')
+    end
+  end
 end
