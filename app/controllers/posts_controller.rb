4bab003bc3ba459af4090549f9bb43712014a646
Samuel Ackon <ackons433@gmail.com>
2022-10-29 07:12:55 +0000

    Add create and new  methods to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,29 @@ class PostsController < ApplicationController
 
   def show
     @post
+    @user
+  end
+
+  def new
+    @user
+    @post = @user.posts.new
+    render :new, locals: { post: @post }
+  end
+
+  def create
+    @user
+    @post = @user.posts.new(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = 'Post created successfully'
+          redirect_to user_post_path(@user)
+        else
+          flash[:error] = 'Error. Unable to create post'
+          render :new, locals: { post: @post }
+        end
+      end
   end
 end
