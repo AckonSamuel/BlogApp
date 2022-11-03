05ded5ec9ea18b3346f79c657182d685375986fd
Samuel Ackon <ackons433@gmail.com>
2022-11-03 13:31:46 +0000

    Add feature test to user show view

---
+++ spec/views/users/user_show_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Users and user page contents', type: :feature do
+  describe 'GET /' do
+    describe 'Check the user show page' do
+      before do
+        @user = User.create(name: 'Itachi', photo: 'https://i.postimg.cc/yx8BkpTj/4.png', bio: 'lojay', posts_counter: 0)
+        @post = Post.create(user: @user, title: 'heloa', comments_counter: 0, likes_counter: 0)
+        visit '/'
+      end
+
+      it 'Check if the users profile picture is present' do
+        visit '/users/1'
+        img = page.find('img')
+        expect(img['src']).to have_content('https://i.postimg.cc/yx8BkpTj/4.png')
+      end
+
+      it 'Check the username' do
+        visit '/users/2'
+        username = page.find('.author')
+        expect(username).to have_content('Post from Itachi')
+      end
+
+      it 'Check the users posts counter' do
+        visit '/users/3'
+        posts_counter = page.find('.user-infos')
+        expect(posts_counter).to have_content('Number of Posts : 1')
+      end
+
+      it 'Check the users bio' do
+        visit '/users/4'
+        bio = page.find('.bio')
+        expect(bio).to have_content('lojay')
+      end
+    end
+  end
+end
