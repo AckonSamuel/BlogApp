63be178ef9b2bb8ee91fca70c090d05b194b17a1
Samuel Ackon <ackons433@gmail.com>
2022-11-04 08:03:14 +0000

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  def current_user
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
