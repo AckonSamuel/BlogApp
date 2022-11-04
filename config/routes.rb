91319d659aba98d476d777874db83838facf17ea
Samuel Ackon <ackons433@gmail.com>
2022-11-04 07:38:03 +0000

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show create new] do
       resources :comments, only: %i[create new]
