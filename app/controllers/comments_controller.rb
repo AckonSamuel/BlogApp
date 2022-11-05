f539e9372d9dd9caf7cd98a90f86c2edffc2d4cf
Samuel Ackon <ackons433@gmail.com>
2022-11-05 01:07:56 +0000

    Add destroy to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
   def create
     post = Post.find(params[:post_id])
     comment = Comment.new(comment_params)
@@ -17,9 +18,19 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = Post.find(@comment.post_id)
+    @post.comments_counter -= 1
+    @comment.destroy
+    @post.save
+    flash[:success] = 'Comment deleted successfully'
+    redirect_to user_post_path(current_user, @post)
+  end
+
   private
 
   def comment_params
-    params.require(:comment).permit(:text)
+    params.require(:comment).permit(:post, :text)
   end
 end
