6a14a36ae234cd5388149565d5f3f46d6b896f26
Samuel Ackon <ackons433@gmail.com>
2022-11-05 01:11:01 +0000

    Add configuration authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,16 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
+     can :manage, :all
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, Post, user_id: user.id
+      can :manage, Comment, user_id: user.id
+      can :read, :all
+    end
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
