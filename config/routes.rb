d995ffea0cd179eadd6ff838a8a9398f0bf33cdc
Samuel Ackon <ackons433@gmail.com>
2022-11-05 01:16:50 +0000

    Refactor routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, sign_out_via: [:get, :post]
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show create new] do
-      resources :comments, only: %i[create new]
+    resources :posts, only: %i[index show create new destroy] do
+      resources :comments, only: %i[create new destroy]
       resources :likes, only: [:create]
     end
   end
---
+++ db/migrate/20221104144619_add_extra_to_users.rb
@@ -0,0 +1,6 @@
+class AddExtraToUsers < ActiveRecord::Migration[7.0]
+    def change
+      add_column :users, :role, :string
+    end
+  end
+  
