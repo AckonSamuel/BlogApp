42301006f3db85ee43900c54a8f9ad27e8191385
Samuel Ackon <ackons433@gmail.com>
2022-11-05 01:03:24 +0000

    Refactor code on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
+  protect_from_forgery with: :exception
   before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :username, :email, :password, :current_password)
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password current_password])
   end
 end
